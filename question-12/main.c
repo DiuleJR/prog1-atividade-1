b90b4465b81dde8ed29b3939e63f3d570dd1d199
DiuleJR <syfras999@gmail.com>
2024-08-03 05:11:16 -0300

    questão 12 - resolvida

---
+++ question-12/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+// Precisei utilizar a biblioteca math.h
+// para poder usar as funções sqrt e pow
+
+// sqrt - obtém a raiz quadrada de um número
+// pow - é usado para calcular a potência de um número
+// %lf - é usado para ler valores do tipo double
+
+int main() {
+
+    double cateto1, cateto2, hipotenusa, somaDosCatetos;
+
+    printf("\n=== Calcula a hipotenusa de um triângulo retângulo ===\n");
+
+    printf("\nDigite o valor da cateto1: ");
+    scanf("%lf", &cateto1);
+
+    printf("\nDigite o valor do cateto2: ");
+    scanf("%lf", &cateto2);
+    
+    somaDosCatetos = pow(cateto1, 2) + pow(cateto2, 2);
+    hipotenusa = sqrt(somaDosCatetos);
+
+    printf("\n===== Resultado =====\n");
+    printf("\nO valor da hipotenusa é igual a %.2f\nOnde o cateto1 é %.2f e o cateto2 é %.2f\n", hipotenusa, cateto1, cateto2);
+    printf("\n===== Resolução =====\n");
+    printf("\nhipotenusa² = cateto1² + cateto2²");
+    printf("\nhipotenusa² = %.2lf² + %.2lf²", cateto1, cateto2);
+    printf("\nhipotenusa² = %.2lf + %.2lf", pow(cateto1, 2), pow(cateto2, 2));
+    printf("\nhipotenusa² = %.2lf", somaDosCatetos);
+    printf("\nhipotenusa = %.2lf \n", hipotenusa);
+    printf("\n=====================\n");
+
+    return 0;
+
+}
