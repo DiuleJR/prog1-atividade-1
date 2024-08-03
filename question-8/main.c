93d320b97e212b5553c828c6bc7bc20c96bb6ce6
DiuleJR <syfras999@gmail.com>
2024-08-03 03:10:27 -0300

    questão 8 resolvida

---
+++ question-8/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main() {
+
+    float C, CC, F;
+
+    printf("\n=== Celsius para Fahrenheit ===\n");
+
+    printf("\nDigite um valor em C°: ");
+    scanf("%f", &C);
+
+    CC = C * 9 / 5;
+    F = CC + 32;
+
+    printf("\n===== Resultado =====\n");
+    printf("\n%.1fC° é equivalente a %.1f°F\n", C, F);
+    printf("\n===== Resolução =====\n");
+    printf("\nF = C x 1,8 + 32 \nF = %.1f x 1,8 + 32 \nF = %.1f + 32 \nF = %.1f \n", C, CC, F);
+    printf("\n=====================\n");
+
+    return 0;
+
+}
